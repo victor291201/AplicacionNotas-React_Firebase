63929f0318939d2bf43d11f56c5c5d0d016b13ab
victor291201 <victor291201hugo@gmail.com>
2021-05-11 11:09:36 -0500

    Updates

---
+++ precache-manifest.d75cfb4a93c657d5ae2b8e5b5d76f216.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5448028cd38302d5f93e80594f4d313e",
+    "url": "/victor291201/AplicacionNotas-React_Firebase/index.html"
+  },
+  {
+    "revision": "d0b94e7e049d044779ab",
+    "url": "/victor291201/AplicacionNotas-React_Firebase/static/css/2.12f5803f.chunk.css"
+  },
+  {
+    "revision": "4601dcbdbd709b112ba9",
+    "url": "/victor291201/AplicacionNotas-React_Firebase/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d0b94e7e049d044779ab",
+    "url": "/victor291201/AplicacionNotas-React_Firebase/static/js/2.7e1235c9.chunk.js"
+  },
+  {
+    "revision": "b7f864985f415c35e187952741b2ee78",
+    "url": "/victor291201/AplicacionNotas-React_Firebase/static/js/2.7e1235c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4601dcbdbd709b112ba9",
+    "url": "/victor291201/AplicacionNotas-React_Firebase/static/js/main.fe251338.chunk.js"
+  },
+  {
+    "revision": "416712222da49141b994",
+    "url": "/victor291201/AplicacionNotas-React_Firebase/static/js/runtime-main.242bc9b7.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/victor291201/AplicacionNotas-React_Firebase/precache-manifest.d75cfb4a93c657d5ae2b8e5b5d76f216.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/victor291201/AplicacionNotas-React_Firebase/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
